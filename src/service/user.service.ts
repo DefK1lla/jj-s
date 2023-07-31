c4bff190f54c48b2727461d1787d07fbe1db0c04
R-serban <nurdavletkr@gmail.com>
2023-07-31 17:47:27 +0500

    fix mvc folder names

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,15 +1,4 @@
-import mongoose from "mongoose";
-interface IUser {
-    username: string;
-    password: string;
-}
-
-const userSchema = new mongoose.Schema<IUser>({
-    username: { type: String, required: true },
-    password: { type: String, required: true },
-});
-
-const User = mongoose.model<IUser>('userAuthentication', userSchema);
+import { User } from "src/model/user.model";
 
 export async function saveUser(username: string, password: string) {
     try{
