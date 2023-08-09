7d47ec498c42b239222796e2da9ab3bf421a8e0e
R-serban <nurdavletkr@gmail.com>
2023-08-09 19:00:56 +0500

    fix auth

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import session from "express-session";
 import bodyParser from 'body-parser';
 import passport from "passport";
+import cookieParser from "cookie-parser";
 
 import authRouter from './routes/auth';
 import fileRouter from './routes/file';
@@ -20,16 +21,22 @@ app.use(
   })
 );
 app.use(express.json());
-app.use(session({
-  secret: "1",
-  resave: false,
-  saveUninitialized: false,
-  
-}));
-
-//app.use(passport.authenticate('session'))
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json())
+app.use(
+  session({
+    secret: [`${process.env.SECRET_KEY}`],
+    resave: true,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: `${process.env.MONGODB_URI}` }),
+    cookie: {
+      sameSite: 'none',
+      secure: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+  })
+);
+app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use('/', authRouter);
@@ -37,28 +44,13 @@ app.use('/', fileRouter);
 app.use('/', interpretrRouter);
 
 app.set("trust proxy", 1);
-// app.use(
-//   session({
-//     secret: `${SECRET}`,
-//     resave: true,
-//     saveUninitialized: true,
-//     store: MongoStore.create({ mongoUrl: MONGODB_URI }),
-//     cookie: {
-//       sameSite: 'none',
-//       secure: true,
-//       maxAge: 1000 * 60 * 60 * 24 * 7,
-//     },
-//   })
-// );
-// app.use(cookieParser());
-// app.use(passport.initialize());
-// app.use(passport.session());
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}!`);
 });
 
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://localhost:27017/test"/*`${process.env.MONGODB_URI}`*/, () =>
+mongoose.connect(`${process.env.MONGODB_URI}`, () =>
   console.log("connected to mongodb")
 );
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,10 +4,12 @@ import * as controller from '../controller/auth.controller'
 
 const router = express.Router();
 
-router.post(`/login`, controller.passportAuthenticate);
+router.post(`/login`, controller.passportAuthenticate, controller.passportAuthenticateCallback);
 
-router.post('/logout', controller.logOut)
+router.get('/logout', controller.logOut)
 
 router.post('/signup', controller.registration)
+
+router.get('/user', controller.getUserData);
 
 export default router;
